6851745923a27bacfb750ab3807324df23faa7b5
Naveen Sampath <ns7767@ad.rit.edu>
2019-10-11 12:07:14 -0500

    Alternate way of using the component syntax

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Book from './Book/Book'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-          <h1>Welcome to Bookshelf app</h1>
-          <p>Currently Reading</p>
-          <Book />
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //       <h1>MyReads</h1>
+    //       <p>Currently Reading</p>
+    //       <Book />
+    //   </div>
+    // );
+
+      return React.createElement('div',{className: App},React.createElement('h1',null,'Myreads'), React.createElement('p',null,'Currently Reading'));
   }
 }
 
