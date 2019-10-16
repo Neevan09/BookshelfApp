4581eebea4d58609050c61e24db56befbcfe099d
Naveen Sampath <ns7767@ad.rit.edu>
2019-10-15 20:08:30 -0500

    Garbage

--- src/Routes/Search.js
+++ src/Routes/Search.js
@@ -3,7 +3,6 @@ import * as BookAPI from '../utils/BooksAPI';
 import Book from "./Book";
 import { Link } from 'react-router-dom'
 
-
 class Search extends Component {
     constructor(props) {
         super(props);
